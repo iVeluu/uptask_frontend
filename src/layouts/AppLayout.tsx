d093f9ad393ed7a4183548ee308c3e623670e8a4
iVeluu <velualex10@gmail.com>
2024-07-05 16:16:23 -0600

    first commit

---
+++ src/layouts/AppLayout.tsx
@@ -0,0 +1,37 @@
+import { Outlet } from "react-router-dom"
+import Logo from "@/components/Logo"
+import NavMenu from "@/components/NavMenu"
+import { ToastContainer } from "react-toastify"
+import 'react-toastify/dist/ReactToastify.css'
+
+
+export default function AppLayout() {
+    return (
+        <>
+            <header className="bg-gray-800 py-5">
+                <div className="max-w-screen-2xl mx-auto flex flex-col md:flex-row lg:flex-row justify-between items-center">
+                    <div className="w-64">
+                        < Logo />
+                    </div>
+                    <NavMenu />
+                </div>
+            </header>
+
+            <section className="mt-10 max-w-screen-2xl mx-auto p-5">
+                <Outlet />
+            </section>
+
+            <footer className="py-5">
+                <p className="text-center">
+                    Todos los derechos reservados { new Date().getFullYear()}
+                </p>
+            </footer>
+
+            <ToastContainer 
+
+            />
+        </>
+
+        
+    )
+}
