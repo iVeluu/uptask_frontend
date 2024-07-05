be6bf5e72571b0c6afd37f76206d16d61755e73d
iVeluu <velualex10@gmail.com>
2024-07-05 17:57:49 -0600

    add display to see the projects

--- src/api/ProjectAPI.ts
+++ src/api/ProjectAPI.ts
@@ -1,11 +1,25 @@
 import api from "@/lib/axios";
-import { ProjectFormData } from "../types";
+import { dashboardProjectSchema, ProjectFormData } from "../types";
 import { isAxiosError } from "axios";
 
 export async function createProject( formData : ProjectFormData ) {
     try {
         const { data } = await api.post('/projects', formData)
         return data
+    } catch (error) {
+       if(isAxiosError(error) && error.response){
+        throw new Error( error.response.data.error)
+       }
+    }
+}
+
+export async function getProjects() {
+    try {
+        const { data } = await api('/projects')
+        const result = dashboardProjectSchema.safeParse(data)
+        if( result.success ) {
+            return result.data
+        }
     } catch (error) {
        if(isAxiosError(error) && error.response){
         throw new Error( error.response.data.error)
