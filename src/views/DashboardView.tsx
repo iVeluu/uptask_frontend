497c2360c7cfac9d6c693ff202ccee509d3952fe
iVeluu <velualex10@gmail.com>
2024-07-07 15:45:48 -0600

    add the function to delete projects

--- src/views/DashboardView.tsx
+++ src/views/DashboardView.tsx
@@ -1,9 +1,10 @@
-import { getProjects } from "@/api/ProjectAPI";
-import { useQuery } from "@tanstack/react-query";
+import { deleteProject, getProjects } from "@/api/ProjectAPI";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { Link } from "react-router-dom";
 import { Fragment } from 'react'
 import { Menu, Transition } from '@headlessui/react'
 import { EllipsisVerticalIcon } from '@heroicons/react/20/solid'
+import { toast } from "react-toastify";
 
 
 export default function DashboardView() {
@@ -11,6 +12,19 @@ export default function DashboardView() {
     const { data } = useQuery({
         queryKey: ['projects'],
         queryFn: getProjects
+    })
+
+    const queryClient = useQueryClient()
+
+    const { mutate } = useMutation({
+        mutationFn: deleteProject,
+        onError: (error) => {
+            toast.error(error.message)
+        },
+        onSuccess: (data) => {
+            toast.success(data)
+            queryClient.invalidateQueries({queryKey: ['projects']})
+        }
     })
 
     if(data) return (
@@ -71,7 +85,7 @@ export default function DashboardView() {
                                                 <button 
                                                     type='button' 
                                                     className='block px-3 py-1 text-sm leading-6 text-red-500'
-                                                    onClick={() => {} }
+                                                    onClick={() => mutate(project._id) }
                                                 >
                                                     Eliminar Proyecto
                                                 </button>
