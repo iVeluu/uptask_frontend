c3daa8b47058dcf5a6b6fd6ed123c17f47b4c199
iVeluu <velualex10@gmail.com>
2024-07-07 15:33:29 -0600

    add the function to edit projects

--- src/api/ProjectAPI.ts
+++ src/api/ProjectAPI.ts
@@ -1,5 +1,5 @@
 import api from "@/lib/axios";
-import { dashboardProjectSchema, ProjectFormData } from "../types";
+import { dashboardProjectSchema, Project, ProjectFormData } from "../types";
 import { isAxiosError } from "axios";
 
 export async function createProject( formData : ProjectFormData ) {
@@ -20,6 +20,33 @@ export async function getProjects() {
         if( result.success ) {
             return result.data
         }
+    } catch (error) {
+       if(isAxiosError(error) && error.response){
+        throw new Error( error.response.data.error)
+       }
+    }
+}
+
+export async function getProjectById( id : Project['_id']) {
+    try {
+        const { data } = await api(`/projects/${id}`)
+        return data
+    } catch (error) {
+       if(isAxiosError(error) && error.response){
+        throw new Error( error.response.data.error)
+       }
+    }
+}
+
+type ProjectAPIType = {
+    formData : ProjectFormData,
+    projectId : Project['_id']
+}
+
+export async function updateProject( { formData, projectId } : ProjectAPIType ) {
+    try {
+        const { data } = await api.put<string>(`/projects/${projectId}`, formData)
+        return data
     } catch (error) {
        if(isAxiosError(error) && error.response){
         throw new Error( error.response.data.error)
