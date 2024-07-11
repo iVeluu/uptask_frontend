873e41f44d447da0ff650e31dd8b82dadd0d6f2a
iVeluu <velualex10@gmail.com>
2024-07-10 18:23:20 -0600

    create the form to confirm the account and the call to the api

--- src/types/index.ts
+++ src/types/index.ts
@@ -7,11 +7,13 @@ const authSchema = z.object({
     email: z.string().email(),
     password: z.string(),
     password_confirmation: z.string(),
+    token: z.string(),
 })
 
 export type Auth = z.infer<typeof authSchema>
 export type UserLoginForm = Pick<Auth, 'email' | 'password'>
 export type UserRegistrationForm = Pick<Auth, 'name' | 'email' | 'password' | 'password_confirmation'>
+export type ConfirmToken = Pick<Auth, 'token'>
 
 /** Tasks */
 export const tastStatusSchema = z.enum(["pending", "onHold", "inProgress", "underReview", "completed"])
