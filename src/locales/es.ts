f44db21f26beec57e800f32b477f8bdff9f5eb24
iVeluu <velualex10@gmail.com>
2024-07-11 21:20:45 -0600

    fix task error

--- src/locales/es.ts
+++ src/locales/es.ts
@@ -2,6 +2,6 @@ export const statusTranslations : { [ key: string ] : string } = {
     pending: 'Pendiente',
     onHold: 'En Espera',
     inProgress: 'En Progreso',
-    underReview: "En Revisión",
+    underPreview: "En Revisión",
     completed: 'Completado'
 }
