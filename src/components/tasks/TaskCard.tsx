0d944ef8a0470d72f9cf4e0550f2ed42f19dc6ea
iVeluu <velualex10@gmail.com>
2024-07-17 19:44:44 -0600

    auth task

--- src/components/tasks/TaskCard.tsx
+++ src/components/tasks/TaskCard.tsx
@@ -9,9 +9,10 @@ import { Fragment } from "react/jsx-runtime"
 
 type TaskCardProps = {
     task: Task
+    canEdit: boolean
 }
 
-export default function TaskCard({ task }: TaskCardProps ) {
+export default function TaskCard({ task, canEdit }: TaskCardProps ) {
 
     const navigate = useNavigate()
 
@@ -35,7 +36,11 @@ export default function TaskCard({ task }: TaskCardProps ) {
     return (
         <li className="p-5 bg-white border border-slate-300 flex justify-between gap-3">
             <div className="min-w-0 flex flex-col gap-y-4">
-                <button type="button" className="text-xl font-bold text-slate-600 text-left">
+                <button 
+                    type="button" 
+                    className="text-xl font-bold text-slate-600 text-left"
+                    onClick={() => navigate(location.pathname + `?viewTask=${task._id}`)}
+                >
                     {task.name}
                 </button>
                 <p className="text-slate-500">{task.description}</p>
@@ -60,25 +65,29 @@ export default function TaskCard({ task }: TaskCardProps ) {
                                     Ver Tarea
                                 </button>
                             </Menu.Item>
-                            <Menu.Item>
-                                <button 
-                                    type='button' 
-                                    className='block px-3 py-1 text-sm leading-6 text-gray-900'
-                                    onClick={() => navigate(location.pathname + `?editTask=${task._id}`)}
-                                >
-                                    Editar Tarea
-                                </button>
-                            </Menu.Item>
+                            {canEdit && (
+                                <>
+                                <Menu.Item>
+                                    <button 
+                                        type='button' 
+                                        className='block px-3 py-1 text-sm leading-6 text-gray-900'
+                                        onClick={() => navigate(location.pathname + `?editTask=${task._id}`)}
+                                    >
+                                        Editar Tarea
+                                    </button>
+                                </Menu.Item>
 
-                            <Menu.Item>
-                                <button 
-                                    type='button' 
-                                    className='block px-3 py-1 text-sm leading-6 text-red-500'
-                                    onClick={() => mutate({projectId, taskId : task._id})}
-                                >
-                                    Eliminar Tarea
-                                </button>
-                            </Menu.Item>
+                                <Menu.Item>
+                                    <button 
+                                        type='button' 
+                                        className='block px-3 py-1 text-sm leading-6 text-red-500'
+                                        onClick={() => mutate({projectId, taskId : task._id})}
+                                    >
+                                        Eliminar Tarea
+                                    </button>
+                                </Menu.Item>
+                                </>
+                            )}
                         </Menu.Items>
                     </Transition>
                 </Menu>
--- src/views/projects/ProjectDetailView.tsx
+++ src/views/projects/ProjectDetailView.tsx
@@ -3,7 +3,10 @@ import AddTaskModal from "@/components/tasks/AddTaskModal"
 import EditTaskData from "@/components/tasks/EditTaskData"
 import TaskList from "@/components/tasks/TaskList"
 import TaskModalDetails from "@/components/tasks/TaskModalDetails"
+import { useAuth } from "@/hooks/useAuth"
+import { isManager } from "@/utils/polices"
 import { useQuery } from "@tanstack/react-query"
+import { useMemo } from "react"
 import { Link, Navigate, useNavigate, useParams } from "react-router-dom"
 
 export default function ProjectDetailView() {
@@ -12,32 +15,38 @@ export default function ProjectDetailView() {
     const params = useParams()
     const projectId = params.projectId!
 
+    const { data: user, isLoading: authLoading } = useAuth()
+
     const { data, isLoading, isError } = useQuery({
         queryKey: ['project', projectId],
         queryFn: () => getProjectById(projectId)
     })
 
-    if (isLoading) return 'Cargando...'
+    const canEdit = useMemo(() => data?.manager === user?._id, [data, user])
+
+    if (isLoading && authLoading) return 'Cargando...'
     if (isError) return <Navigate to={'/404'} />
-    if (data) return (
+    if (data && user) return (
         <>
             <h1 className="text-5xl font-black">{data.projectName}</h1>
             <p className="text-2xl font-light text-gray-500 mt-5">{data.description}</p>
 
-            <nav className="my-5 flex gap-3">
-                <button 
-                    type="button"
-                    className="bg-purple-400 hover:bg-purple-500 transition-colors px-10 py-3 text-white text-xl font-bold cursor-pointer"
-                    onClick={() => navigate( location.pathname + '?newTask=true')}
-                    >Agregar Tarea</button>
+            {isManager(data.manager, user._id) && (
+                <nav className="my-5 flex gap-3">
+                    <button 
+                        type="button"
+                        className="bg-purple-400 hover:bg-purple-500 transition-colors px-10 py-3 text-white text-xl font-bold cursor-pointer"
+                        onClick={() => navigate( location.pathname + '?newTask=true')}
+                        >Agregar Tarea</button>
 
-                <Link
-                    to={'team'}
-                    className="bg-fuchsia-400 hover:bg-fuchsia-500 transition-colors px-10 py-3 text-white text-xl font-bold cursor-pointer"
-                >Colaboradores</Link>
-            </nav>
+                    <Link
+                        to={'team'}
+                        className="bg-fuchsia-400 hover:bg-fuchsia-500 transition-colors px-10 py-3 text-white text-xl font-bold cursor-pointer"
+                    >Colaboradores</Link>
+                </nav>
+            )}
 
-            <TaskList tasks={data.tasks} />
+            <TaskList tasks={data.tasks} canEdit={canEdit }/>
             <AddTaskModal />
             <EditTaskData />
             <TaskModalDetails/>
